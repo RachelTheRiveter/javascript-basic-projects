42b31fc06cab3fcdde8ff230126cb4128e85a718
RachelTheRiveter <theholthut@gmail.com>
2021-04-16 23:39:05 -0600

    finished

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,34 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(q){
+  // console.log(q);
+  const btn = q.querySelector('.question-btn');
+  // console.log(btn);
+
+  btn.addEventListener('click', function() {
+
+    // if click does not match the expanded article, close the other article
+    questions.forEach(function(i){
+      if(i !== q){
+        i.classList.remove('show-text');
+      }
+    });
+
+    q.classList.toggle('show-text');
+  });
+});
+
+
+
 // traversing the dom
+
+// const btns = document.querySelectorAll('.question-btn');
+//
+// btns.forEach(function (btn) {
+//   btn.addEventListener('click', function (e) {
+//       const question = e.currentTarget.parentElement.parentElement;
+//       question.classList.toggle('show-text');
+//   });
+// });
